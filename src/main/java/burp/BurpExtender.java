90d2739eb6964e014d343b69c7cdba6a8d0c36b3
whwlsfb <whwlsfb@wanghw.cn>
2022-09-02 10:45:34 +0800

    v0.12

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -14,7 +14,7 @@ public class BurpExtender implements IBurpExtender, IExtensionStateListener {
     public IBurpExtenderCallbacks callbacks;
     public PrintWriter stdout;
     public PrintWriter stderr;
-    public String version = "0.11";
+    public String version = "0.12";
     public Log4j2ScanUIHandler uiHandler;
     public Log4j2Scanner scanner;
 
