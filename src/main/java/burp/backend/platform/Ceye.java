406ca31dd5827a4a23dad79a11f65ec173a419ad
whwlsfb <whwlsfb@wanghw.cn>
2022-11-09 16:06:48 +0800

    opt ceye backend.

--- src/main/java/burp/backend/platform/Ceye.java
+++ src/main/java/burp/backend/platform/Ceye.java
@@ -7,6 +7,7 @@ import burp.utils.HttpUtils;
 import burp.utils.Utils;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
 
@@ -20,6 +21,7 @@ public class Ceye implements IBackend {
     String platformUrl = "http://api.ceye.io/";
     String rootDomain;
     String token;
+    String cache = "";
 
     public Ceye() {
         this.rootDomain = Config.get(Config.CEYE_IDENTIFIER);
@@ -30,10 +32,12 @@ public class Ceye implements IBackend {
     public boolean supportBatchCheck() {
         return false;
     }
+
     @Override
     public String[] batchCheck(String[] payloads) {
         return new String[0];
     }
+
     @Override
     public String getName() {
         return "Ceye.io";
@@ -46,17 +50,7 @@ public class Ceye implements IBackend {
 
     @Override
     public boolean CheckResult(String domain) {
-        try {
-            Response resp = client.newCall(HttpUtils.GetDefaultRequest(platformUrl + "v1/records?token=" + token + "&type=dns&filter=" + domain.toLowerCase().substring(0, domain.indexOf("."))).build()).execute();
-            JSONObject jObj = JSONObject.parseObject(resp.body().string().toLowerCase());
-            if (jObj.containsKey("data")) {
-                return (((JSONArray) jObj.get("data")).size() > 0);
-            }
-        } catch (Exception ex) {
-            System.out.println(ex);
-            return false;
-        }
-        return false;
+        return cache.contains(domain);
     }
 
     @Override
@@ -66,7 +60,14 @@ public class Ceye implements IBackend {
 
     @Override
     public boolean flushCache() {
-        return true;
+        try {
+            Response resp = client.newCall(HttpUtils.GetDefaultRequest(platformUrl + "v1/records?token=" + token + "&type=dns").build()).execute();
+            cache = resp.body().string().toLowerCase();
+            return true;
+        } catch (Exception ex) {
+            System.out.println(ex);
+            return false;
+        }
     }
 
     @Override
